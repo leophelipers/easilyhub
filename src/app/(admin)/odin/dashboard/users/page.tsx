6c40449cfc24acf36dff8043e04dc81b62ea16b4
Leo Phelipe <leo.phe.rod@gmail.com>
2024-09-26 14:53:06 -0300

    deploy problem 1

--- src/app/(admin)/odin/dashboard/users/page.tsx
+++ src/app/(admin)/odin/dashboard/users/page.tsx
@@ -24,9 +24,6 @@ export const payments: Payment[] = [
   // ...
 ]
 
-export const description =
-  'An products dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. It displays a list of products in a table with actions.'
-
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col ">
