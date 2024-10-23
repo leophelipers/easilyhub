7505966e95a6a2964bf2c35630cdd864bae46853
Leo Phelipe <leo.phe.rod@gmail.com>
2024-10-23 17:26:49 -0300

    teste git

---
+++ src/app/(unprotected)/lp/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+// export interface IexampleProps {} props: IAdminTopbarProps
+
+export default function example() {
+  return (
+    <div>
+      <h1>example</h1>
+    </div>
+  )
+}
