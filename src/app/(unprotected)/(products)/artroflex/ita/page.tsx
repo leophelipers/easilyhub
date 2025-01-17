1911ade89cb5d9361bdcea6438a78bca04144b65
Leo Phelipe <leo.phe.rod@gmail.com>
2025-01-17 15:39:28 -0300

    new products

---
+++ src/app/(unprotected)/(products)/artroflex/ita/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import CookieNoticeItalia from '../../../_components/cookiesItalia/CookiesItalia'
+
+// export interface IAppProps {} props: IAppProps
+
+export default function App() {
+  return (
+    <div>
+      <CookieNoticeItalia iframeSrc="https://it.artroflexactive.com/?gtag=11564217017&utm_source=213183" />
+    </div>
+  )
+}
---
+++ src/app/(unprotected)/(products)/parazax/ita/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import CookieNoticeItalia from '../../../_components/cookiesItalia/CookiesItalia'
+
+// export interface IAppProps {} props: IAppProps
+
+export default function App() {
+  return (
+    <div>
+      <CookieNoticeItalia iframeSrc="https://it2.parazaxoriginal.com/?gtag=11564217017&utm_source=213179" />
+    </div>
+  )
+}
