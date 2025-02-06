80b634c1ef476b0e76055e6b8acff1305a327454
Leo Phelipe <leo.phe.rod@gmail.com>
2025-02-06 19:32:04 -0300

    correct portugal

--- src/app/(unprotected)/(products)/cardiobalance/pt/page.tsx
+++ src/app/(unprotected)/(products)/cardiobalance/pt/page.tsx
@@ -1,12 +1,35 @@
 'use client'
+
+import { useMutation } from 'convex/react'
+import { useEffect } from 'react'
+import { api } from '../../../../../../convex/_generated/api'
 import CookieNoticePortugal from '../../../_components/cookiesportugal/CookiesPortugal'
 
-// export interface IAppProps {} props: IAppProps
-
 export default function App() {
+  const saveClick = useMutation(api.drcashclicks.create)
+
+  useEffect(() => {
+    const getUrlParameter = (name: string): string | null => {
+      const urlParams = new URLSearchParams(window.location.search)
+      return urlParams.get(name)
+    }
+
+    const gclid = getUrlParameter('gclid')
+
+    if (gclid) {
+      saveClick({
+        gclid,
+        fullUrl: window.location.href,
+      })
+    }
+  }, [saveClick])
+
   return (
-    <div>
-      <CookieNoticePortugal iframeSrc="https://pt.cardiobalanceoriginal.com/?gtag=11564217017&utm_source=213979" />
+    <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
+      <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+      <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
+        <CookieNoticePortugal iframeSrc="https://xlg5u.doctortrf.com/l" />
+      </div>
     </div>
   )
 }
