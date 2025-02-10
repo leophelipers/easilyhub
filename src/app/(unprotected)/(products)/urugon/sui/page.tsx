1b512abab72a9beaae8f003640ec61613126c0fa
Leo Phelipe <leo.phe.rod@gmail.com>
2025-02-10 11:56:32 -0300

    new product 4 geos

---
+++ src/app/(unprotected)/(products)/urugon/sui/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import CookieNoticeSchweiz from '@/app/(unprotected)/_components/cookiessuica/CookiesSuica'
+import { useMutation } from 'convex/react'
+import { useEffect } from 'react'
+import { api } from '../../../../../../convex/_generated/api'
+
+export default function App() {
+  const saveClick = useMutation(api.drcashclicks.create)
+
+  useEffect(() => {
+    const getUrlParameter = (name: string): string | null => {
+      const urlParams = new URLSearchParams(window.location.search)
+      return urlParams.get(name)
+    }
+
+    const gclid = getUrlParameter('gclid')
+
+    if (gclid) {
+      saveClick({
+        gclid,
+        fullUrl: window.location.href,
+      })
+    }
+  }, [saveClick])
+
+  return (
+    <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
+      <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+      <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
+        <CookieNoticeSchweiz iframeSrc="https://ch10.urogun.com/?utm_source=216905" />
+      </div>
+    </div>
+  )
+}
---
+++ src/app/(unprotected)/_components/cookiessuica/CookiesSuica.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/no-unescaped-entities */
+'use client'
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+interface CookieNoticeProps {
+  iframeSrc: string
+}
+
+export default function CookieNoticeSchweiz({ iframeSrc }: CookieNoticeProps) {
+  return (
+    <main className="relative w-screen h-screen overflow-hidden">
+      {/* Hintergrund-Iframe */}
+
+      {/* Cookie-Hinweis */}
+      <div className="fixed inset-0 text-black flex items-center justify-center p-4 z-50">
+        <div className="bg-background rounded-lg shadow-2xl max-w-md w-full p-6">
+          <h2 className="text-xl font-semibold mb-4">Cookie-Richtlinie</h2>
+          <p className="text-sm mb-6">
+            Diese Website verwendet Cookies, um die Navigation zu verbessern und
+            respektiert Ihre Privatsphäre gemäß der DSGVO. Ihre Daten werden
+            ohne Ihre ausdrückliche Zustimmung nicht verwendet. Für weitere
+            Details lesen Sie bitte unsere Datenschutzrichtlinie. Verwalten Sie
+            Ihre Präferenzen oder klicken Sie auf „Akzeptieren“, um die Nutzung
+            von Cookies zu erlauben{' '}
+            <Link href={iframeSrc} passHref={true}>
+              <Button variant={'ghost'}>Cookie-Richtlinie</Button>
+            </Link>
+            .
+          </p>
+          <div className="flex justify-end space-x-4">
+            <Link href={iframeSrc} passHref={true}>
+              <Button variant="outline" className="whitespace-nowrap">
+                Ablehnen
+              </Button>
+            </Link>
+            <Link href={iframeSrc} passHref={true}>
+              <Button className="whitespace-nowrap">Akzeptieren</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* Optional: Overlay für Inhalte */}
+      <div className="relative z-10 p-4">
+        {/* Zusätzliche Inhalte, die über dem Iframe erscheinen sollen */}
+      </div>
+    </main>
+  )
+}
