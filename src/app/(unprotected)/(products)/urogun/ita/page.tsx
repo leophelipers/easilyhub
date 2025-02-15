82f62580b9e4846a2699df181f44dc519ab9dbd6
Leo Phelipe <leo.phe.rod@gmail.com>
2025-02-15 16:56:19 -0300

    cookies change

--- src/app/(unprotected)/(products)/urogun/ita/page.tsx
+++ src/app/(unprotected)/(products)/urogun/ita/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import CookieLeadItalia from '@/app/(unprotected)/_components/cookiesleaditalia/CookiesLeadItalia'
+import CookieNoticeItalia from '@/app/(unprotected)/_components/cookiesItalia/CookiesItalia'
 import { useMutation } from 'convex/react'
 import { useEffect } from 'react'
 import { api } from '../../../../../../convex/_generated/api'
@@ -28,7 +28,7 @@ export default function App() {
     <div className='relative h-screen w-full bg-[url("/urogun.png")] bg-cover bg-center'>
       <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
       <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
-        <CookieLeadItalia iframeSrc="https://it21.urogun.com/?utm_source=216903" />
+        <CookieNoticeItalia iframeSrc="https://it21.urogun.com/?utm_source=216903" />
       </div>
     </div>
   )
