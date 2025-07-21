8d12030eb695961184a3988d4d07aba6ff3f860b
Leo Phelipe <leo.phe.rod@gmail.com>
2025-07-21 16:32:16 -0300

    hondro sol

---
+++ src/app/(unprotected)/(products)/hondrosol/ita/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useMutation } from 'convex/react'
+import { useEffect } from 'react'
+import { api } from '../../../../../../convex/_generated/api'
+import CookieNoticeItalia from '../../../_components/cookiesItalia/CookiesItalia'
+
+export default function App() {
+  const saveClick = useMutation(api.drcashclicks.create)
+
+  useEffect(() => {
+    const getUrlParameter = (name: string): string | null => {
+      const urlParams = new URLSearchParams(window.location.search)
+      return urlParams.get(name)
+    }
+
+    const gclid = getUrlParameter('gclid')
+
+    if (gclid) {
+      saveClick({
+        gclid,
+        fullUrl: window.location.href,
+      })
+    }
+  }, [saveClick])
+
+  return (
+    <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
+      <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+      <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
+        <CookieNoticeItalia iframeSrc="https://it4.doctorhealthpro.com/?utm_source=1591" />
+      </div>
+    </div>
+  )
+}
