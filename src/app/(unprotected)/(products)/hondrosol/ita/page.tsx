6891e044c3e47d7583431bb76018496d4b1bea09
Leo Phelipe <leo.phe.rod@gmail.com>
2025-07-22 19:59:33 -0300

    insulinet e hondrosol

--- src/app/(unprotected)/(products)/hondrosol/ita/page.tsx
+++ src/app/(unprotected)/(products)/hondrosol/ita/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useMutation } from 'convex/react'
+import Head from 'next/head'
 import { useEffect } from 'react'
 import { api } from '../../../../../../convex/_generated/api'
 import CookieNoticeItalia from '../../../_components/cookiesItalia/CookiesItalia'
@@ -25,11 +26,33 @@ export default function App() {
   }, [saveClick])
 
   return (
-    <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
-      <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
-      <div className="relative z-11 flex justify-center items-center h-full text-white text-2xl ">
-        <CookieNoticeItalia iframeSrc="https://it4.doctorhealthpro.com/?utm_source=1591" />
+    <>
+      <Head>
+        {/*
+          Usando dangerouslySetInnerHTML para injetar um script inline.
+          Este script inline cria dinamicamente a tag do script do Filtripixel e a adiciona ao <head>.
+          Este método é uma alternativa robusta quando o <Script> do Next.js não funciona como esperado
+          com certos scripts de terceiros, forçando o carregamento de forma mais direta.
+        */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var script = document.createElement('script');
+                script.src = 'https://static.filtripixel.io/filtripixel.minify.js?pixelId=9c45903b-8fef-4f32-b253-bb4c8c868e10';
+                script.async = true;
+                document.head.appendChild(script);
+              })();
+            `,
+          }}
+        />
+      </Head>
+      <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
+        <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+        <div className="relative z-11 flex justify-center items-center h-full text-white text-2xl ">
+          <CookieNoticeItalia iframeSrc="https://it4.doctorhealthpro.com/?utm_source=1591" />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/app/(unprotected)/(products)/insulinet/po/page.tsx
+++ src/app/(unprotected)/(products)/insulinet/po/page.tsx
@@ -2,6 +2,7 @@
 
 import CookieNoticePoland from '@/app/(unprotected)/_components/cookiespolonia/CookiesPolonia'
 import { useMutation } from 'convex/react'
+import Script from 'next/script'
 import { useEffect } from 'react'
 import { api } from '../../../../../../convex/_generated/api'
 
@@ -25,11 +26,19 @@ export default function App() {
   }, [saveClick])
 
   return (
-    <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
-      <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
-      <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
-        <CookieNoticePoland iframeSrc="https://pl.doctorbodyfuel.com/?utm_source=1349" />
+    <>
+      <Script
+        data-render-head="true"
+        id="filtripixel-tracking"
+        src="https://static.filtripixel.io/filtripixel.minify.js?pixelId=d6931546-5f6a-40ca-bc4d-e9fd6c8e2cfd"
+        strategy="beforeInteractive"
+      />
+      <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
+        <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+        <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
+          <CookieNoticePoland iframeSrc="https://pl.doctorbodyfuel.com/?utm_source=1349" />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
