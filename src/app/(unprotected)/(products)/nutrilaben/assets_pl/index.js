5448cefbd0c5967064cbce47c2b99137558782c9
Leo Phelipe <leo.phe.rod@gmail.com>
2025-11-03 12:13:55 -0300

    vitofina

---
+++ src/app/(unprotected)/(products)/nutrilaben/assets_pl/index.js
@@ -0,0 +1,28 @@
+$(document).on('click','a[href^="#"]',function(event){event.preventDefault()
+$('html, body').animate({scrollTop:$($.attr(this,'href')).offset().top,},1000)})
+const months=['Stycznia','Lutego','Marca','Kwietnia','Maja','Czerwca','Lipca','Sierpnia','Września','Października','Listopada','Grudnia',];(monthMin=['','','','','','','','','','','','']),(days=['duminică','luni','marți','miercuri','joi','vineri','sâmbătă']),(daysMin=['','','','','','','']),(seasons=['iarnă','arc','vară','toamnă'])
+function postDate(daysName,daysMinName,monthsName,monthsMinName,seasonsName){const _counterLength=60
+for(let counter=0;counter<_counterLength;counter++){innerDate(counter,'date-')
+innerDate(counter,'date')}
+function innerDate(counter,dateType){let newCounter
+dateType==='date-'?(newCounter=-counter):(newCounter=counter)
+const _msInDay=86400000,_localDate=new Date(Date.now()+newCounter*_msInDay),_day=_localDate.getDate(),_month=_localDate.getMonth()+1,_year=_localDate.getFullYear()
+const dayDefault=addZero(_day),monthDefault=addZero(_month),defaultDate=dayDefault+'.'+monthDefault+'.'+_year
+const dateClass=dateType+counter,nodeList=document.querySelectorAll('.'+dateClass)
+for(let i=0;i<nodeList.length;i++){const dateFormat=nodeList[i].dataset.format
+dateFormat!==undefined&&dateFormat!==''?(nodeList[i].innerHTML=String(changeFormat(dayDefault,_month,_year,dateFormat,newCounter))):(nodeList[i].innerHTML=defaultDate)}}
+function changeFormat(_day,_month,_year,format,counter){let innerFormat=format
+const testFormat=['dd','mm','yyyy','monthFull','monthOnly','year'],dateFormat={dd:_day,mm:addZero(_month),yyyy:_year,monthFull:getMonthName(_month,monthsName,false),monthOnly:getMonthName(_month,monthsName,false,counter),year:getYearWithCounter(_year,counter),}
+for(let i=0;i<testFormat.length;i++){let string=testFormat[i]
+let regExp=new RegExp(string)
+innerFormat=innerFormat.replace(regExp,dateFormat[string])}
+return innerFormat.split(' ').join(' ')}
+function getMonthName(_month,monthsName,bigFirstLetter,counter){const monthCounter=!!counter?counter:0
+let month
+_month+monthCounter>12?(month=monthCounter-(12-_month)):(month=_month+monthCounter)
+_month+monthCounter<=0?(month=12+monthCounter+1):(month=_month+monthCounter)
+return changeFirstLetter(bigFirstLetter,monthsName[month-1])}
+function getYearWithCounter(year,counter){return year+counter}
+function addZero(numb){return numb<10?'0'+numb:numb}
+function changeFirstLetter(isBig,str){return isBig&&str&&str.length>0?str[0].toUpperCase()+str.slice(1):str}}
+if(document.body.classList.contains('ev-date')){document.addEventListener('DOMContentLoaded',function(){postDate(days,daysMin,months,monthMin,seasons)})}
