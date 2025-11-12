ef94b5401b6105cfb4846628fe90a42ce16dea50
Leo Phelipe <leo.phe.rod@gmail.com>
2025-11-12 17:59:17 -0300

    memomaster

---
+++ src/app/(unprotected)/(products)/memomaster/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useMutation } from 'convex/react'
+import { useEffect } from 'react'
+import { api } from '../../../../../../convex/_generated/api'
+import CookieNoticeEnglish from '../../../_components/cookiesUS/CookiesUs'
+
+export default function App() {
+  const saveClick = useMutation(api.drcashclicks.create)
+
+  useEffect(() => {
+    const getUrlParameter = (name: string): string | null => {
+      const urlParams = new URLSearchParams(window.location.search)
+      return urlParams.get(name)
+    }
+
+    const gclid = getUrlParameter('gclid')
+
+    if (gclid) {
+      saveClick({
+        gclid,
+        fullUrl: window.location.href,
+      })
+    }
+  }, [saveClick])
+
+  return (
+    <div className='relative h-screen w-full bg-[url("/moon.png")] bg-cover bg-center'>
+      <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+      <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
+        <CookieNoticeEnglish iframeSrc="https://cvk4n.doctormurin.com/l" />
+      </div>
+    </div>
+  )
+}
