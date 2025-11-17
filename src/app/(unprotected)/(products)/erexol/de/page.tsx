012573c74b48ffccbc166580da3ae8f9a0b9e559
Leo Phelipe <leo.phe.rod@gmail.com>
2025-11-17 12:19:21 -0300

    erexol de

---
+++ src/app/(unprotected)/(products)/erexol/de/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useMutation } from 'convex/react'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import { api } from '../../../../../../convex/_generated/api'
+import CookieNoticeAlemanha from '../../../_components/cookiesAlemanha/CookiesAlemanha'
+
+export default function App() {
+  const saveClick = useMutation(api.drcashclicks.create)
+
+  useEffect(() => {
+    const getUrlParameter = (name: string): string | null => {
+      const urlParams = new URLSearchParams(window.location.search)
+      return urlParams.get(name)
+    }
+
+    const gclid = getUrlParameter('gclid')
+
+    if (gclid) {
+      saveClick({
+        gclid,
+        fullUrl: window.location.href,
+      })
+    }
+  }, [saveClick])
+
+  return (
+    <>
+      {/* Este script é carregado com a estratégia "beforeInteractive".
+        Isso garante que ele seja injetado e executado antes de qualquer código React 
+        da página, tornando-o uma das primeiras coisas a carregar.
+        <!-- Start Filtripixel tracking (filtripixel.minify.js) -->
+<!-- Place at the end of the head and before the </head> tag -->
+<script data-render-head="true" src="https://static.filtripixel.io/filtripixel.minify.js?pixelId=093a8a03-2b72-4081-a57e-80bf2adf42ba"></script>
+<!-- End Filtripixel tracking (filtripixel.minify.js) -->
+      */}
+      <Script
+        data-render-head="true"
+        id="filtripixel-tracking"
+        src="https://static.filtripixel.io/filtripixel.minify.js?pixelId=093a8a03-2b72-4081-a57e-80bf2adf42ba"
+        strategy="beforeInteractive"
+      />
+
+      <div className='relative h-screen w-full bg-[url("/ultravix.png")] bg-cover bg-center'>
+        <div className="absolute inset-0 backdrop-blur-lg bg-black/30"></div>
+        <div className="relative z-10 flex justify-center items-center h-full text-white text-2xl">
+          <CookieNoticeAlemanha iframeSrc="https://aiz0q.doctormurin.com/l" />
+        </div>
+      </div>
+    </>
+  )
+}
